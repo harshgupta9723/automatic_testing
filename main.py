44e80167beb14fb4ce217d4e929bbde8da55337b
harshgupta9723 <harshg9723@gmail.com>
2021-03-23 18:11:06 +0530

    test

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-x = "hello"
+from flask import Flask, request, jsonify
+from datetime import datetime
 
-try:
-    print(x)
-except Exception as e:
-    print("Exception in jbo2location: "e)
+
+def get_result(sample_input):
+    x = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
+    final = 'today' + x
+    return final
+
+
+app = Flask(__name__)
+
+@app.route('/',methods=['POST','GET'])
+def education_level():
+
+    sample_input = request.form.get("input")
+    result = get_result(sample_input)
+    result_dict = {"updated" : result }
+    return jsonify(result_dict)
+
+if __name__ == "__main__":
+    app.run(port=5080)
