09fc6aa1a6294723bc94b01f698bddb6e322f3f3
harshgupta9723 <harshg9723@gmail.com>
2021-03-26 13:36:42 +0530

    test

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def get_result(sample_input):
 
 app = Flask(__name__)
 
-@app.route('/',methods=['POST','GET'])
+@app.route('/', methods=['POST', 'GET'])
 def education_level():
 
     sample_input = request.form.get("input")
