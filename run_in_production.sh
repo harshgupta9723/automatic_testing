389cfe155c8355be007a20b919b353bda3098cb7
harshgupta9723 <harshg9723@gmail.com>
2021-03-27 22:43:27 +0530

    test

--- run_in_production.sh
+++ run_in_production.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+cd /home/harsh/rank_estimator
+
 # Activate virtual environment
 source venv/bin/activate
 
 # Install requirements.txt
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 
 # run API
-python3 main.py
+python main.py
