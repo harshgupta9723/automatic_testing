3edda5280890ebccd6462ebe219e0348e9f6178a
harshgupta9723 <harshg9723@gmail.com>
2021-03-30 22:07:07 +0530

    test

--- run_in_production.sh
+++ run_in_production.sh
@@ -2,10 +2,17 @@
 
 cd /home/harsh/rank_estimator
 
+sudo apt-get install python3-venv
+
 python3 -m venv venv
 
 source venv/bin/activate
 
+touch text.py
+
+# Activate virtual environment
+# source venv/bin/activate
+
 # Install requirements.txt
-pip3 install -r requirements.txt
+# pip install -r requirements.txt
 
