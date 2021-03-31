885f4d0b52a6832289a70657d4f08a762ce4ad53
harshgupta9723 <harshg9723@gmail.com>
2021-03-31 22:46:44 +0530

    test

--- testing_folder_2/testing_3/testing_2.py
+++ testing_folder_2/testing_3/testing_2.py
@@ -1,15 +1,23 @@
 from flask import Flask, request, jsonify
 from datetime import datetime
+
+
 def get_result(sample_input):
     x = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
     final = 'today' + x
     return final
+
+
 app = Flask(__name__)
+
+
 @app.route('/', methods=['POST', 'GET'])
 def education_level():
     sample_input = request.form.get("input")
     result = get_result(sample_input)
     result_dict = {"updated_test": result}
     return jsonify(result_dict)
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     app.run(port=5080)
