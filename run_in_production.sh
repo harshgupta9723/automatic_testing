ff64b55de3dff1091f6e1cc82e63b41fc797c025
harshgupta9723 <harshg9723@gmail.com>
2021-03-31 09:37:29 +0530

    test

--- run_in_production.sh
+++ run_in_production.sh
@@ -2,14 +2,19 @@
 
 cd /home/harsh/rank_estimator
 
+python3 -m pip install virtualenv
 
 python3 -m venv venv
 
-source venv/bin/activate
-
 # Activate virtual environment
-# source venv/bin/activate
+source venv/bin/activate
 
 # Install requirements.txt
 pip3 install -r requirements.txt
 
+# activate(){
+#     . ./venv/bin/activate
+#     python3 main.py
+# }
+
+# activate
